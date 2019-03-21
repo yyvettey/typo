c6aacd024590efa0a2d68a87f90eb25418e258b7
Ye Yuan <ye.yuan@tamu.edu>
2019-03-21 09:31:17 +0000

    Bug Fix

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -63,4 +63,24 @@ describe Admin::CategoriesController do
     assert_raise(ActiveRecord::RecordNotFound) { Category.find(test_id) }
   end
   
+  describe "test_create" do 
+    before(:each) do
+     get :new
+   end
+
+   it 'should render template new' do
+     assert_template 'new'
+     assert_tag :tag => "table",
+       :attributes => { :id => "category_container" }
+   end
+
+   it 'should create a new category' do
+     post :edit, :category => {:name => "Inception", :keywords => "scifi", :permalink => "NA", :description => "wonderful" }
+     assert_response :redirect, :action => "index"
+     expect(assigns(:category)).not_to be_nil
+     expect(flash[:notice]).to eq("Category was successfully saved.")
+   end
+   
+ end 
+  
 end
